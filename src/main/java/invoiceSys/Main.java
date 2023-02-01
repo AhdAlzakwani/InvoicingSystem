26f0cb40b5b1e89e8e9d55d8ebfa757f03d89a2b
AHD ALZAKWNI <116621706+AhdAlzakwani@users.noreply.github.com>
2023-02-01 17:06:44 +0400

    using HashMap For program Statistic

--- src/main/java/invoiceSys/Main.java
+++ src/main/java/invoiceSys/Main.java
@@ -1,7 +1,9 @@
 package invoiceSys;
 
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.InputMismatchException;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -13,6 +15,13 @@ import shopServices.Constants;
 import shopServices.InvoiceDetails;
 
 public class Main {
+	public static HashMap<String, Integer> countMenuSelected(String caseName, Integer count) {
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		map.put(caseName, count);
+		count = count++;
+		return map;
+	}
 
 	public static void main(String[] args)
 			throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
@@ -21,8 +30,8 @@ public class Main {
 		AllShopServices allShopServices = new AllShopServices();
 		AllItemServices allItemServices = new AllItemServices();
 		InvoiceDetails details = new InvoiceDetails();
-		Stack<String> programStatistic = new Stack<String>();
-
+		String caseName = "";
+		Integer count = 1;
 		boolean exitFlagMainMenu = true;
 		boolean exitFlagShopSittingMenu = true;
 		boolean exitFlagShopItemSittingMenu = true;
@@ -67,18 +76,23 @@ public class Main {
 					System.out.println("************************");
 					Integer optionForMainMenu = 0;
 					try {
-						 optionForMainMenu = scannerMenu.nextInt();
+						optionForMainMenu = scannerMenu.nextInt();
 					} catch (InputMismatchException Exception) {
-						System.out.println("Wroong Input !!!! You Should Use Only String Input"+Exception);
+						System.out.println("Wroong Input !!!! You Should Use Only String Input" + Exception);
 
 					}
 					String soptionForMainMenu = String.valueOf(optionForMainMenu);
-					programStatistic.push(soptionForMainMenu);
 					switch (optionForMainMenu) {
 					case 0:
+						 caseName = "Connect to Database";
+						 count = 1;
+						countMenuSelected(caseName,count);
 						Constants.getDatabaseConnection(userName, soptionForMainMenu, userPassword);
 						break;
 					case 1:
+						 caseName = "Shop Settings";
+						 count = 1;
+						countMenuSelected(caseName,count);
 						while (exitFlagShopSittingMenu) {
 
 							for (String x : Menu.getShopSettings()) {
@@ -88,9 +102,15 @@ public class Main {
 							System.out.println("************************");
 							System.out.println("Select Only One Option :");
 							System.out.println("************************");
-							Integer optionForShopItemSittingMenu = scannerMenu.nextInt();
+							Integer optionForShopItemSittingMenu = 0;
+							try {
+								optionForShopItemSittingMenu = scannerMenu.nextInt();
+							} catch (InputMismatchException Exception) {
+								System.out.println("Wroong Input !!!! You Should Use Only String Input" + Exception);
+
+							}
+
 							String soptionForShopItemSittingMenu = String.valueOf(optionForShopItemSittingMenu);
-							programStatistic.push(soptionForShopItemSittingMenu);
 							switch (optionForShopItemSittingMenu) {
 							case 0:
 								allShopServices.createShopTable(Constants.USER_URL, Constants.USER_NAME,
@@ -99,26 +119,55 @@ public class Main {
 										Constants.USER_PASSWORD);
 								break;
 							case 1:
+								 
+								 caseName = "Load Data (Items and invoices)";
+								 count = 1;
+								countMenuSelected(caseName,count);
+								Integer InsertOption = 0;
 								System.out.println(
 										"Do you want to insert Items? If yes, press 1 , if you want to insert invoices, then press 2");
-								Integer InsertOption = scannerMenu.nextInt();
+
+								try {
+									InsertOption = scannerMenu.nextInt();
+								} catch (InputMismatchException Exception) {
+									System.out
+											.println("Wroong Input !!!! You Should Use Only Integer Input" + Exception);
+
+								}
 								String sInsertOption = String.valueOf(InsertOption);
-								programStatistic.push(sInsertOption);
 								if (InsertOption == 1) {
 
 									System.out.println("Please Enter Shop_Name :");
-									String shop_name = scannerMenu.next();
-									programStatistic.push(shop_name);
+									String shop_name = "";
+									try {
+										shop_name = scannerMenu.next();
+									} catch (InputMismatchException Exception) {
+										System.out.println(
+												"Wroong Input !!!! You Should Use Only String Input" + Exception);
+
+									}
 									allItemServices.insertIntoItemTable(shop_name, Constants.USER_URL,
 											Constants.USER_NAME, Constants.USER_PASSWORD);
 								} else if (InsertOption == 2) {
 									System.out.println("How many Invoice You want to Insert ?");
-									Integer InvoiceNumberToInsert = scannerMenu.nextInt();
+									Integer InvoiceNumberToInsert = 0;
+									try {
+										InvoiceNumberToInsert = scannerMenu.nextInt();
+									} catch (InputMismatchException Exception) {
+										System.out.println(
+												"Wroong Input !!!! You Should Use Only Integer Input" + Exception);
+
+									}
 									String sInvoiceNumberToInsert = String.valueOf(InvoiceNumberToInsert);
-									programStatistic.push(sInvoiceNumberToInsert);
 									System.out.println("Please Enter Item Name :");
-									String Item_Name = scannerMenu.next();
-									programStatistic.push(Item_Name);
+									String Item_Name = "";
+									try {
+										Item_Name = scannerMenu.next();
+									} catch (InputMismatchException Exception) {
+										System.out.println(
+												"Wroong Input !!!! You Should Use Only String Input" + Exception);
+
+									}
 									details.insertIntoShopDetailsTable(InvoiceNumberToInsert, Item_Name,
 											Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
 
@@ -129,29 +178,56 @@ public class Main {
 
 								break;
 							case 2:
+								 caseName = "Set Shop Name (data should be saved)";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								System.out.println("How many Shop You want to Insert ?");
-								Integer shopNumberToInsert = scannerMenu.nextInt();
+								Integer shopNumberToInsert = 0;
+								try {
+									shopNumberToInsert = scannerMenu.nextInt();
+								} catch (InputMismatchException Exception) {
+									System.out
+											.println("Wroong Input !!!! You Should Use Only Integer Input" + Exception);
+
+								}
 								String sshopNumberToInsert = String.valueOf(shopNumberToInsert);
-								programStatistic.push(sshopNumberToInsert);
 
 								allShopServices.insertIntoShopTable(shopNumberToInsert, Constants.USER_URL,
 										Constants.USER_NAME, Constants.USER_PASSWORD);
 
 								break;
 							case 3:
+								caseName = "Set Invoice Header (Tel / Fax / Email / Website) (Data should be saved)";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								System.out.println("How many Shop You want to Insert ?");
-								Integer shopDetailsNumberToInsert = scannerMenu.nextInt();
+								Integer shopDetailsNumberToInsert = 0;
+								try {
+									shopDetailsNumberToInsert = scannerMenu.nextInt();
+								} catch (InputMismatchException Exception) {
+									System.out
+											.println("Wroong Input !!!! You Should Use Only Integer Input" + Exception);
+
+								}
 								String sshopDetailsNumberToInsert = String.valueOf(shopDetailsNumberToInsert);
-								programStatistic.push(sshopDetailsNumberToInsert);
 
 								System.out.println("Please Enter Shop_Name :");
-								String shop_name = scannerMenu.next();
-								programStatistic.push(shop_name);
+								String shop_name = "";
+								try {
+									shop_name = scannerMenu.next();
+
+								} catch (InputMismatchException Exception) {
+									System.out
+											.println("Wroong Input !!!! You Should Use Only String Input" + Exception);
+
+								}
 								allShopServices.insertIntoShopDetailsTable(shopDetailsNumberToInsert, shop_name,
 										Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
 
 								break;
 							case 4:
+								caseName = "Go Back";
+								countMenuSelected(caseName,count);
 								exitFlagMainMenu = true;
 								exitFlagShopSittingMenu = false;
 								break;
@@ -163,6 +239,9 @@ public class Main {
 
 						break;
 					case 2:
+						caseName = "Manage Shop Items";
+						 count = 1;
+						countMenuSelected(caseName,count);
 						while (exitFlagShopItemSittingMenu) {
 
 							for (String x : Menu.getShopItemSettings()) {
@@ -175,50 +254,63 @@ public class Main {
 							Integer optionForShopSittingMenu = scannerMenu.nextInt();
 
 							String soptionForShopSittingMenu = String.valueOf(optionForShopSittingMenu);
-							programStatistic.push(soptionForShopSittingMenu);
 
 							switch (optionForShopSittingMenu) {
 							case 0:
+								caseName = "Create Items Table";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								allItemServices.createItemTable(Constants.USER_URL, Constants.USER_NAME,
 										Constants.USER_PASSWORD);
 								break;
 							case 1:
-
+								caseName = "Add Items";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								System.out.println("Please Enter Shop_Name :");
 								String shop_name = scannerMenu.next();
-								programStatistic.push(shop_name);
 
 								allItemServices.insertIntoItemTable(shop_name, Constants.USER_URL, Constants.USER_NAME,
 										Constants.USER_PASSWORD);
 
 								break;
 							case 2:
+								caseName = "Delete Items";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								System.out.println(" Enter id to be deleted ?");
 								int id = scannerMenu.nextInt();
 								String sid = String.valueOf(id);
-								programStatistic.push(sid);
 								AllItemServices.deleteItemsById(id, Constants.USER_URL, Constants.USER_NAME,
 										Constants.USER_PASSWORD);
 								break;
 							case 3:
+								caseName = "Change Item Price";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								System.out.println(" Enter id to be Update ?");
 								int itemId = scannerMenu.nextInt();
 								String sitemId = String.valueOf(itemId);
-								programStatistic.push(sitemId);
 								System.out.println(" Enter price to be Update ?");
 								int itemprice = scannerMenu.nextInt();
 								String sitemprice = String.valueOf(itemprice);
-								programStatistic.push(sitemprice);
 								AllItemServices.updateItemsPriceById(itemId, itemprice, Constants.USER_URL,
 										Constants.USER_NAME, Constants.USER_PASSWORD);
 
 								break;
 							case 4:
+								caseName = "Report All Items";
+								 count = 1;
+								countMenuSelected(caseName,count);
 								allItemServices.readFromTable(Constants.USER_URL, Constants.USER_NAME,
 										Constants.USER_PASSWORD);
 
 								break;
 							case 5:
+								caseName = "Go Back";
+								 count = 1;
+
+								countMenuSelected(caseName,count);
 								exitFlagMainMenu = true;
 								exitFlagShopItemSittingMenu = false;
 								break;
@@ -230,36 +322,51 @@ public class Main {
 
 						break;
 					case 3:
+						caseName = "Create New Invoice";
+						 count = 1;
+						countMenuSelected(caseName,count);
 						details.createInvoiceTable(Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
 
 						break;
 					case 4:
-						details.reportTable(Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
+						caseName = "Report (No Of Items, No of Invoices, Total Sales)";
+						 count = 1;
+						countMenuSelected(caseName,count);						details.reportTable(Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
 						break;
 					case 5:
+						caseName = "Report: All Invoices";
+						 count = 1;
+						countMenuSelected(caseName,count);	
 						details.readFromTable(Constants.USER_URL, Constants.USER_NAME, Constants.USER_PASSWORD);
 
 						break;
 					case 6:
+						caseName = "Search (1) Invoice";
+						 count = 1;
+						countMenuSelected(caseName,count);
 						System.out.println(" Enter id to be Selected ?");
 						int invoiceId = scannerMenu.nextInt();
 						String sinvoiceId = String.valueOf(invoiceId);
-						programStatistic.push(sinvoiceId);
 						details.readFromInvoiceById(invoiceId, Constants.USER_URL, Constants.USER_NAME,
 								Constants.USER_PASSWORD);
 
 						break;
 					case 7:
-						try {
-							Stack<String> st = (Stack<String>) programStatistic;
-							System.out.println(st);
-						} catch (Exception e) {
-							System.out.println(e.getMessage());
-						}
-//				Stack<String> st = (Stack<String>) programStatistic.pop();
-//				System.out.println("Stack POP: " + programStatistic.pop()); 
+						caseName = "Program Statistics ";
+						 count = 1;
+						countMenuSelected(caseName,count);
+						System.out.println(countMenuSelected(caseName,count));
+						
+						for(Map.Entry<String,Integer> entry:countMenuSelected(caseName,count).entrySet()){
+							
+		                    System.out.println("   "+ entry.getKey() +"  " + entry.getValue());
+		    }
+						
 						break;
 					case 8:
+						caseName = "Exit";
+						count=1 ;
+						countMenuSelected(caseName,count);
 						System.out.println(
 								"Are you sure you want to exit? If yes, program ends, if No , then main menu reprinted again");
 						String optionFlagMainMenu = scannerMenu.next();
